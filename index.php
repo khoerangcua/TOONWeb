ade876c6383e5684a2330c08ab269e3802a7ae44
Bảo Đặng <baodang.31191023095@st.ueh.edu.vn>
2022-04-04 19:17:42 +0700

    cap nhat style css

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="public/style/style.css">
+    
 </head>
 <body>
-    <p>TOON WEB</p>
+    <button class="bt1"></button>
 </body>
 </html>
