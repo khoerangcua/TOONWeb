6c19516fd0dff2743812cb174afb5395eea6d844
Bảo Đặng <baodang.31191023095@st.ueh.edu.vn>
2022-04-04 19:29:28 +0700

    update style.css

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <!--google icon-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+    <!--font chữ-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <!--my style-->
     <link rel="stylesheet" href="public/style/style.css">
     
 </head>
 <body>
     <button class="bt1"></button>
+    <p class="h2">Alo 123</p>
+    <span class="material-icons">search</span>
+    <span class="material-icons md-18">search</span>
+
+
+    
 </body>
 </html>
