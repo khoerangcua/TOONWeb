b8fc76054d7e50ea76a19299071d192dd2749e55
duyyfinal <duynguyen.31191025723@st.ueh.edu.vn>
2022-04-07 14:11:31 +0700

    cn12
    Duy

---
+++ public/page/chitiettincanmua.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="../style/style.css">
+<title>Chi tiết tin cần mua</title>
+</head>
+
+<body>
+	<style>
+		body{background-color: #FFD954;}
+	</style>
+	<div class="container" >
+		<div class="row " >
+			<p class="tieudeMNCDT">Chi tiết Tin</p>
+		</div>
+		<div class="row rowchitiettincanmua" >
+			<div class="col-md-2" >
+				<img class="imgchitietMNCDT"  src="../images/harrypotter_ylup.jpg" alt="Hình ảnh sách cần mua">
+			</div>
+			<div class="col-md-10">
+				<div class="row" style="margin-top: 25px " >Harry Potter</div>
+				<div class="row" style="margin-top: 5px">Thể  loại: Phiêu lưu</div>
+				<div class="row" style="margin-top: 5px">Tác giả: Jack Thorne</div>
+				<div class="row" style="margin-top: 5px">Mô tả: Đây là phần mới nhất của bộ truyện Harry Potter. Tầm giá khoảng 50.000 vnd.Bạn nào có thì liên hệ ngay với mình bằng số điện thoại bên dưới hoặc vào trang của mình để biết nhiều thông tin hơn. Giá cả có thể thương lượng.</div>
+			</div>
+		</div>
+		<div class="row" style="height: 100px"></div>
+		<div class="row">
+			<div class="col-md-6 ">
+				<div class="row" style="height: 100px; background-color: #969393">
+					<div class="col-md-1">
+						<img class="hinh_tron" src="../images/275225020_3180982448841648_1157249981496616579_n.png">
+					</div>
+					<div class="col-md-3">
+						<div class="row" style="margin-top: 5px ">Vô đối</div>
+						<div class="row" style="margin-top: 5px ">0123456456</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ public/page/pageMNCDT.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="../style/style.css">
+
+<title>MNCDT</title>
+</head>
+
+<body>
+	<style>
+		body{background-color: #FFD954;}
+	</style>
+	<div class="container" style="background-color: #FFD954">
+		<div class="row tieudeMNCDT" >
+			<p style="text-align: center">Danh sách các tin cần mua</p>
+		</div>
+		
+		<div class="row rowlisttincanmua" >
+			<div class="col-md-1" >
+				<img class="imglistMNCDT"  src="../images/harrypotter_ylup.jpg" alt="Hình ảnh sách cần mua">
+			</div>
+			<div class="col-md-11">
+				<div class="row" style="margin-top: 15px " >Harry Potter</div>
+				<div class="row" style="margin-top: 5px">Mình đang cần mua cuốn này gấp. Ai có liên hệ mình</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
