c7996fb77bae8d137ce9168c74aa441fbf974e73
duyyfinal <duynguyen.31191025723@st.ueh.edu.vn>
2022-04-21 09:02:44 +0700

    update
    duy

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once("private/Controllers/Switchpagecontroller.php");
+$_REQUEST
 $switchpagecontroler = new SwichPageControler();
 $switchpagecontroler->SwitchPage();
 ?>
