d8f811910734b6e0ed48306fd2e42b6cf3b600b8
duyyfinal <duynguyen.31191025723@st.ueh.edu.vn>
2022-05-14 18:03:02 +0700

    duy
    update timkiem

--- private/models/baidangban_model.php
+++ private/models/baidangban_model.php
@@ -81,7 +81,7 @@ public function LoadBaiDangBanMoi() {
     $baidangban = array();
     $link = "";
     taoKetNoi( $link );
-    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM tbl_baidangban AS bdban WHERE bdban.trangthai = 1 AND bdban.thoigian >= '$ngay'" );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM tbl_baidangban AS bdban WHERE bdban.trangthai = 1 AND bdban.thoigian >= '$ngay' ORDER BY bdban.thoigian DESC" );
     while ( $rows = mysqli_fetch_assoc( $result ) ) {
       array_push( $baidangban, $rows );
     }
@@ -91,7 +91,7 @@ public function LoadBaiDangBanMoi() {
     $theloai = array();
     $link = "";
     taoKetNoi( $link );
-    $result = chayTruyVanTraVeDL( $link, "SELECT DISTINCT theloai FROM tbl_baidangban ORDER BY theloai ASC" );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM `tbl_theloai` ORDER BY tentheloai ASC" );
     while ( $rows = mysqli_fetch_assoc( $result ) ) {
       array_push( $theloai, $rows );
 
@@ -99,17 +99,110 @@ public function LoadBaiDangBanMoi() {
     giaiPhongBoNho( $link, $result );
     return $theloai;
   }
-	public function LoadBaiDangBanTheoLoai($theloai) {
+	public function LoadBaiDangBanTheoLoai($idtheloai) {
     $baidangban = array();
     $link = "";
     taoKetNoi( $link );
-    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM tbl_baidangban WHERE tbl_baidangban.trangthai = -1 AND tbl_baidangban.theloai=$theloai" );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM (SELECT tbl_baidangban.*, tbl_theloai.tentheloai, tbl_theloai.anhloai FROM tbl_baidangban INNER JOIN tbl_theloai ON tbl_baidangban.idtheloai = tbl_theloai.idtheloai) AS bdban WHERE bdban.trangthai=1 AND bdban.idtheloai = $idtheloai" );
     while ( $rows = mysqli_fetch_assoc( $result ) ) {
       array_push( $baidangban, $rows );
 
     }
     giaiPhongBoNho( $link, $result );
     return $baidangban;
+  }
+	public function LoadBaiDangTuThanhTimKiem($key) {
+    $baidangban = array();
+	$mangrong = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM (SELECT tbl_baidangban.*, tbl_theloai.tentheloai, tbl_theloai.anhloai FROM tbl_baidangban INNER JOIN tbl_theloai ON tbl_baidangban.idtheloai = tbl_theloai.idtheloai) AS bdban WHERE (bdban.trangthai = 1 AND bdban.tensach LIKE '%".$key."%') OR (bdban.trangthai = 1 AND bdban.tentheloai LIKE '%".$key."%')" );
+    if ( $result == true ) {
+      while ( $rows = mysqli_fetch_assoc( $result ) ) {
+        array_push( $baidangban, $rows );
+
+      }
+      giaiPhongBoNho( $link, $result );
+      return $baidangban;
+    } else {
+      return $mangrong;
+    }
+  }
+	public function LoadBaiDangBanFillter($tinhthanh, $domoi, $gia) {
+		$query = "SELECT * FROM (SELECT tbl_baidangban.*, bang2.tentinhthanh, bang2.idtinhthanh FROM tbl_baidangban INNER JOIN (SELECT tbl_taikhoan.*, tbl_tinhthanh.tentinhthanh FROM tbl_taikhoan INNER JOIN tbl_tinhthanh ON tbl_taikhoan.idtinhthanh = tbl_tinhthanh.idtinhthanh) AS bang2 ON tbl_baidangban.idtaikhoan = bang2.idtaikhoan) AS bang1 ";
+		if($tinhthanh !=-1 || $domoi !=-1 || $gia !=-1){
+			$query .="WHERE bang1.trangthai = 1 AND";
+			if ($tinhthanh != -1) {
+				$query .= " ( ";
+				for ($i = 0; $i < count($tinhthanh); $i++) {
+
+					if ($i == 0) {
+						$query .= " bang1.idtinhthanh = " . $tinhthanh[$i] . " ";
+						
+					} else {
+						$query .= " OR bang1.idtinhthanh = " . $tinhthanh[$i] . " ";
+						
+					}
+				}
+				$query .= " ) ";
+			}
+
+			if ($domoi != -1) {
+
+				for ($i = 0; $i < count($domoi); $i++) {
+					if ($i == 0 && $tinhthanh != -1) {
+						$query .= " And (bang1.chatluong " . $domoi[$i] . " ";
+						
+					} else {
+						if ($i == 0 && !$tinhthanh != -1) {
+							$query .= " (bang1.chatluong " . $domoi[$i] . " ";
+							
+						} else {
+							$query .= " OR bang1.chatluong " . $domoi[$i] . " ";
+							
+						}
+					}
+				}
+				$query .= " ) ";
+			}
+
+			if ($gia != -1) {
+				for ($i = 0; $i < count($gia); $i++) {
+
+					if ($i == 0 && $domoi != -1 || $tinhthanh != -1) {
+						$query .= " And ( bang1.gia " . $gia[$i] . " ";
+						
+					} else {
+						if ($i == 0 && !($domoi != -1) || $tinhthanh != -1) {
+							$query .= " ( bang1.gia " . $gia[$i] . " ";
+							
+						} else {
+							$query .= " OR bang1.gia " . $gia[$i] . " ";
+							
+						}
+					}
+					
+				}
+				$query .= " ) ";
+			}
+
+			
+		}
+    $baidangban = array();
+	$mangrong = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, $query);
+    if ( $result == true ) {
+      while ( $rows = mysqli_fetch_assoc( $result ) ) {
+        array_push( $baidangban, $rows );
+
+      }
+      giaiPhongBoNho( $link, $result );
+      return $baidangban;
+    } else {
+      return $mangrong;
+    }
   }
 }
 ?>
--- private/models/tincanmua_model.php
+++ private/models/tincanmua_model.php
@@ -65,5 +65,133 @@ class TinCanMuaModel {
     }
     return $tincanmua[ 0 ];
   }
+	public function LoadTinCanMuaMoi() {
+	$ngay = date("Y-m-d",mktime(0,0,0, date("m")-2, date("d"), date("Y")));
+    $tincanmua = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM tbl_tincanmua AS tcmua WHERE tcmua.trangthai = 1 AND tcmua.thoigian >= '$ngay' ORDER BY tcmua.thoigian DESC" );
+    while ( $rows = mysqli_fetch_assoc( $result ) ) {
+      array_push( $tincanmua, $rows );
+    }
+    return $tincanmua;
+  }
+	public function LoadDanhMucTheLoai() {
+    $theloai = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM `tbl_theloai` ORDER BY tentheloai ASC" );
+    while ( $rows = mysqli_fetch_assoc( $result ) ) {
+      array_push( $theloai, $rows );
+
+    }
+    giaiPhongBoNho( $link, $result );
+    return $theloai;
+  }
+	public function LoadTinCanMuaTheoLoai($idtheloai) {
+    $tincanmua = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM (SELECT tbl_tincanmua.*, tbl_theloai.tentheloai, tbl_theloai.anhloai FROM tbl_tincanmua INNER JOIN tbl_theloai ON tbl_tincanmua.idtheloai = tbl_theloai.idtheloai) AS tcmua WHERE tcmua.trangthai=1 AND tcmua.idtheloai = $idtheloai" );
+    while ( $rows = mysqli_fetch_assoc( $result ) ) {
+      array_push( $tincanmua, $rows );
+
+    }
+    giaiPhongBoNho( $link, $result );
+    return $tincanmua;
+  }
+	public function LoadBaiDangTuThanhTimKiem($key) {
+    $tincanmua = array();
+	$mangrong = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, "SELECT * FROM (SELECT tbl_tincanmua.*, tbl_theloai.tentheloai, tbl_theloai.anhloai FROM tbl_tincanmua INNER JOIN tbl_theloai ON tbl_tincanmua.idtheloai = tbl_theloai.idtheloai) AS tcmua WHERE (tcmua.trangthai = 1 AND tcmua.tensach LIKE '%".$key."%') OR (tcmua.trangthai = 1 AND tcmua.tentheloai LIKE '%".$key."%')" );
+    if ( $result == true ) {
+      while ( $rows = mysqli_fetch_assoc( $result ) ) {
+        array_push( $tincanmua, $rows );
+
+      }
+      giaiPhongBoNho( $link, $result );
+      return $tincanmua;
+    } else {
+      return $mangrong;
+    }
+  }
+	public function LoadTinCanMuaFillter($tinhthanh, $domoi, $gia) {
+		$query = "SELECT * FROM (SELECT tbl_tincanmua.*, bang2.tentinhthanh, bang2.idtinhthanh FROM tbl_tincanmua INNER JOIN (SELECT tbl_taikhoan.*, tbl_tinhthanh.tentinhthanh FROM tbl_taikhoan INNER JOIN tbl_tinhthanh ON tbl_taikhoan.idtinhthanh = tbl_tinhthanh.idtinhthanh) AS bang2 ON tbl_tincanmua.idtaikhoan = bang2.idtaikhoan) AS bang1 ";
+		if($tinhthanh !=-1 || $domoi !=-1 || $gia !=-1){
+			$query .="WHERE bang1.trangthai = 1 AND";
+			if ($tinhthanh != -1) {
+				$query .= " ( ";
+				for ($i = 0; $i < count($tinhthanh); $i++) {
+
+					if ($i == 0) {
+						$query .= " bang1.idtinhthanh = " . $tinhthanh[$i] . " ";
+						
+					} else {
+						$query .= " OR bang1.idtinhthanh = " . $tinhthanh[$i] . " ";
+						
+					}
+				}
+				$query .= " ) ";
+			}
+
+			if ($domoi != -1) {
+
+				for ($i = 0; $i < count($domoi); $i++) {
+					if ($i == 0 && $tinhthanh != -1) {
+						$query .= " And (bang1.chatluongmax " . $domoi[$i] . " ";
+						
+					} else {
+						if ($i == 0 && !$tinhthanh != -1) {
+							$query .= " (bang1.chatluongmax " . $domoi[$i] . " ";
+							
+						} else {
+							$query .= " OR bang1.chatluongmax " . $domoi[$i] . " ";
+							
+						}
+					}
+				}
+				$query .= " ) ";
+			}
+
+			if ($gia != -1) {
+				for ($i = 0; $i < count($gia); $i++) {
+
+					if ($i == 0 && $domoi != -1 || $tinhthanh != -1) {
+						$query .= " And ( bang1.giamax " . $gia[$i] . " ";
+						
+					} else {
+						if ($i == 0 && !($domoi != -1) || $tinhthanh != -1) {
+							$query .= " ( bang1.giamax " . $gia[$i] . " ";
+							
+						} else {
+							$query .= " OR bang1.giamax " . $gia[$i] . " ";
+							
+						}
+					}
+					
+				}
+				$query .= " ) ";
+			}
+
+			
+		}
+    $tincanmua = array();
+	$mangrong = array();
+    $link = "";
+    taoKetNoi( $link );
+    $result = chayTruyVanTraVeDL( $link, $query);
+    if ( $result == true ) {
+      while ( $rows = mysqli_fetch_assoc( $result ) ) {
+        array_push( $tincanmua, $rows );
+
+      }
+      giaiPhongBoNho( $link, $result );
+      return $tincanmua;
+    } else {
+      return $mangrong;
+    }
+  }
 }
 ?>
